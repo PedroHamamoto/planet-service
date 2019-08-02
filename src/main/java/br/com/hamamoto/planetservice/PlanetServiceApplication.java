4bfb011194ae4571b9f3a662b30fbffff4cd953d
PedroHamamoto <pedrohamamoto@hotmail.com>
2019-08-02 18:29:59 -0300

    first commit

---
+++ src/main/java/br/com/hamamoto/planetservice/PlanetServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.hamamoto.planetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetServiceApplication.class, args);
+	}
+
+}
